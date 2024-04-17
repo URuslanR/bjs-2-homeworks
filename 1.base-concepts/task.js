08b804446340a8769d31f6d0002170b1dca4850a
Ruslan Yusupov <u.ruslan@bk.ru>
2024-04-17 09:33:33 +0700

    Exerise 1 Edit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,23 @@
 "use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let d;
+  let discriminant = b ** 2 - 4 * a * c;
 
-  d = b ** 2 - 4 * a * c;
-
-  if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-  } else if (d === 0) {
+  if (discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+  } else if (discriminant === 0) {
     arr[0] = -b / (2 * a);
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P, n, S, paymant, totalAmount;
 
-  P = percent / 1200;
-  n = countMonths;
-  S = amount - contribution;
-
-  paymant = S * (P + P / ((1 + P) ** n - 1));
-  totalAmount = paymant * n;
+  let creditBody = amount - contribution;
+  let percentBet = percent /= 1200;
+  let paymant = creditBody * (percentBet + percentBet / ((1 + percentBet) ** countMonths - 1));
+  let totalAmount = paymant * countMonths;
+   
   return Number(totalAmount.toFixed(2));
 }
